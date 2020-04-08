8c3a6d05db126c290ac015a5dbdefe679681a55c
Lucas Pagliarin <lucas.pagliarin@gmail.com>
2020-04-08 18:38:35 -0300

    Implementation of a middleware that verifies if the ID is valid and sends the repoIndex via request.params

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -9,6 +9,25 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+
+function isIdValid(req, res, next) {
+  const { id } = req.params;
+
+  if (!isUuid(id)){
+    return res.status(400).json({error: 'Invalid ID'})
+  }
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+  
+  if (repoIndex < 0){
+    return res.status(400).json({ error: "Repository not found."});
+  }
+
+  req.params.repoIndex = repoIndex;
+
+  return next();
+
+}
 
 app.get("/repositories", (request, response) => {
   return response.json(repositories);
@@ -30,16 +49,9 @@ app.post("/repositories", (request, response) => {
   return response.json(repo);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  const { id } = request.params;
+app.put("/repositories/:id", isIdValid, (request, response) => {
+  const { id, repoIndex } = request.params;
   const { title, url, techs } = request.body;
-  
-  const repoIndex = repositories.findIndex(repo => repo.id === id);
-  
-  
-  if (repoIndex < 0){
-    return response.status(400).json({ error: "Repository not found."});
-  }
 
   const { likes } = repositories[repoIndex];
 
@@ -56,28 +68,16 @@ app.put("/repositories/:id", (request, response) => {
   return response.json(repo);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  const { id } = req.params;
-
-  const repoIndex = repositories.findIndex(repo => repo.id === id);
-
-  if (repoIndex < 0){
-    return res.status(400).json({ error: "Repository not found."});
-  }
+app.delete("/repositories/:id", isIdValid, (req, res) => {
+  const { repoIndex } = req.params;
 
   repositories.splice(repoIndex, 1);
 
   return res.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  const { id } = request.params;
-
-  const repoIndex = repositories.findIndex(repo => repo.id === id);
-
-  if (repoIndex < 0){
-    return response.status(400).json({ error: "Repository not found."});
-  }
+app.post("/repositories/:id/like", isIdValid, (request, response) => {
+  const { id, repoIndex } = request.params;
 
   const { likes, title, url, techs } = repositories[repoIndex];
 
